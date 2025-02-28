4b752805f23df393325ab4d486c2baba4386a11d
Szymon Mikulski <simmik@poczta.onet.pl>
2025-02-28 15:20:21 +0100

    Update dict - add 'Pasmanteria'

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 #Zadanie 1
 zakupy = {
     "piekarnia": ["chleb", "bułki", "pączek", "rogal", "bagietka", "drożdżówka"],
-    "warzywniak": ["marchew", "seler", "rukola", "szczypiorek gruby", "papryka"]
+    "warzywniak": ["marchew", "seler", "rukola", "szczypiorek gruby", "papryka"],
+    "pasmanteria": ["igła"]
 }
 
 print("LISTA ZAKUPÓW")
@@ -11,6 +12,6 @@ for sklep, produkty in zakupy.items():
     print(f"Idę do {sklep.capitalize()} i kupuję tam {produkty}")
 
 liczbaproduktow = sum(len(produkty) for produkty in zakupy.values())
-print(f"W sumie kupuję {liczbaproduktow} produktów.")
 
+print(f"W sumie kupuję {liczbaproduktow} produktów.")
 print("Miłego dnia!")
