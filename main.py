8b00ef05c68f83c4d159df3f39fe7b8835cbf77a
Szymon Mikulski <simmik@poczta.onet.pl>
2025-02-28 13:37:42 +0100

    Add code body from prev file

--- main.py
+++ main.py
@@ -3,3 +3,11 @@ zakupy = {
     "piekarnia": ["chleb", "bułki", "pączek"],
     "warzywniak": ["marchew", "seler", "rukola"]
 }
+
+print("LISTA ZAKUPÓW")
+for sklep, produkty in zakupy.items():
+    produkty = [produkt.capitalize() for produkt in produkty]
+    print(f"Idę do {sklep.capitalize()} i kupuję tam {produkty}")
+
+liczbaproduktow = sum(len(produkty) for produkty in zakupy.values())
+print(f"W sumie kupuję {liczbaproduktow} produktów.")
